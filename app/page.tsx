444cbc27090b93460ef5f84a4eb9fe41117aea65
Chirag Laxminarayan Agarwal <57190871+ChiragAGA@users.noreply.github.com>
2026-04-19 15:57:34 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { createServerSupabase } from "@/lib/supabase";
+import { createServerSupabase } from "@/lib/supabase-server";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const supabase = createServerSupabase();
-  const { data } = await supabase.auth.getUser();
+const supabase = await createServerSupabase();
+const { data } = await supabase.auth.getUser();
 
   if (data.user) {
     redirect("/dashboard");
